454c611e58babc440346f60e733da20d1b021818
Rodryg0 <72528257+Rodryg0@users.noreply.github.com>
2020-10-20 20:13:50 -0500

    Update Exhaustivo.py

--- Exhaustivo.py
+++ Exhaustivo.py
@@ -2,31 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sympy import Symbol
 from scipy.misc import derivative 
-import math
 
-def dorada(xu,xl,f):
+def exhaustiva(xk,xk_n,f):
     k = 0
-    R=(math.sqrt(5)-1)/2  
-    while (xu - xl) > .1 :
-        d = R*(xu-xl)
-        x1 = xl + d
-        x2 = xu - d
-        k = k + 1         
-        print('k= ', k,' x1 ',x1,' f(x1) ',f(x1),' x2 ',x2,' f(x2) ',f(x2),' d ',d)
-        if f(x1) < f(x2):
-            xl=x2
-        else:
-            xu=x1  
-    if f(x1) < f(x2):
-        return x1
-    else:
-        return x2
-    return x
-     
-xl=0
-xu=4          
+    while f(xk_n) < f(xk):
+        xk=xk_n
+        xk_n= xk_n + h
+        k += 1
+        print('k= ', k, ' xk = ',xk, ' f(xk)=',f(xk),' xk_n ',xk_n,' f(xk_n)= ',f(xk_n))
+    return xk  
+              
+x=1
+h=0.1             
 f=lambda x:x**2-np.sin(x)
-x=dorada(xu,xl,f)
+x=exhaustiva(x,h,f)
 y=f(x)
 print(x," ",y)
 
@@ -34,3 +23,5 @@ X = np.linspace(0,4,100)
 Y= f(X)
 plt.plot(X,Y)
 plt.plot(x,y,"o")
+              
+        
